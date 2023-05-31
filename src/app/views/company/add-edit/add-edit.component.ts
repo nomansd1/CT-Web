943d8c714cfa9e956cb682cbf139746f7a9ace51
Noman Ali <noman.sd114@gmail.com>
2023-05-31 07:15:11 +0500

    improving global styles

--- src/app/views/company/add-edit/add-edit.component.ts
+++ src/app/views/company/add-edit/add-edit.component.ts
@@ -7,10 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AddEditComponent {
   navigationMenu: any = [
-    { label: 'Principle', type: 'button', class: 'btn primary__btn mx-1'},
-    { label: 'Bulk Edit', type: 'button', class: 'btn primary__btn mx-1'},
-    { label: 'Cancel Edit', type: 'button', class: 'btn danger__btn mx-1 !hidden'},
-    { label: 'Edit', type: 'button', class: 'btn outline__btn mx-1'},
-    { label: 'New', type: 'button', class: 'btn outline__btn mx-1'},
+    { type: 'input', class: ''},
+    { label: 'Principle', type: 'button', class: 'btn primary__btn mx-1', icon: 'Sell'},
+    { label: 'Bulk Edit', type: 'button', class: 'btn primary__btn mx-1', icon: 'edit_note'},
+    { label: 'Cancel Edit', type: 'button', class: 'btn danger__btn mx-1 !hidden', icon: 'edit'},
+    { label: 'Edit', type: 'button', class: 'btn outline__btn mx-1', icon: 'edit'},
+    { label: 'New', type: 'button', class: 'btn outline__btn mx-1', icon: 'add'},
+
   ]
 }
